6ca249af3c7b67633ebc504afb413f0da08861bb
jefferson-bit <jhonsonbabi@hotmail.com>
2021-05-26 10:59:57 -0300

    05.transação-do-cartão

---
+++ src/main/java/br/com/zup/jefferson/transacao/evento/EventoDeTransacao.java
@@ -0,0 +1,10 @@
+package br.com.zup.jefferson.transacao.evento;
+
+import java.math.BigDecimal;
+
+public class EventoDeTransacao {
+    @SuppressWarnings("unused")
+	private String id;
+    @SuppressWarnings("unused")
+	private BigDecimal valor;
+}
